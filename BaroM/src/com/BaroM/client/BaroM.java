25802fd07f390f6f5c93ca05ac1303c34e5c1970
Margo <maqqu@hotmail.com>
2016-01-12 18:57:52 +0200

    Updating BaroM java.

--- BaroM/src/com/BaroM/client/BaroM.java
+++ BaroM/src/com/BaroM/client/BaroM.java
@@ -11,17 +11,15 @@ public class BaroM implements EntryPoint {
 
 	public void onModuleLoad() {
 
-		//HTML html = new HTML("This is made by <b>Margo Allik</b>", true);
+		// HTML html = new HTML("This is made by <b>Margo Allik</b>", true);
 
-				
-		    	String startToken = History.getToken();
-		    	
-			    if (startToken.length() == 0) {
-			      History.newItem("ManuOne");
-			    }
-			    
-		    	History.addValueChangeHandler(new MyHistoryListener());
-	    	History.fireCurrentHistoryState(); 
-		    }
+		String startToken = History.getToken();
+
+		if (startToken.length() == 0) {
+			History.newItem("ManuOne");
 		}
-		 
+
+		History.addValueChangeHandler(new MyHistoryListener());
+		History.fireCurrentHistoryState();
+	}
+}
